035c74bdb9bb840571b7ed54a396ee89c9ae5d2d
Pallav Parikh <pallavjparikh@gmail.com>
2023-06-27 00:49:00 +0530

    Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Kernel for Universal8895-common
+rm -rf kernel/samsung/universal8895
+git clone https://github.com/8890q/android_kernel_samsung_universal8895.git -b lineage-20.0 kernel/samsung/universal8895 --depth 1 --single-branch
+
+# Common tree for Universal8895-common
+rm -rf device/samsung/universal8895-common
+git clone https://github.com/8890q/android_device_samsung_universal8895-common.git -b lineage-20.0 device/samsung/universal8895-common--depth 1
+
+# Vendor blobs for Note 8
+rm -rf vendor/samsung
+git clone https://github.com/8890q/proprietary_vendor_samsung.git -b lineage-20.0 vendor/samsung --depth 1
+
+# Hardware OSS parts for Samsung
+rm -rf hardware/samsung
+git clone https://github.com/ProjectElixir-Devices/hardware_samsung hardware/samsung --depth 1 -b thirteen
+
+# SLSI Sepolicy
+rm -rf device/samsung_slsi/sepolicy
+git clone https://github.com/roynatech2544/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy --depth 1
