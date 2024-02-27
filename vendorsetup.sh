d4e41b329587f082bf04aef3bb73389651fc9cff
Pallav Parikh <pallavjparikh@gmail.com>
2024-02-26 17:22:12 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,20 +2,20 @@
 
 # Kernel for Universal8895-common
 rm -rf kernel/samsung/universal8895
-git clone https://github.com/8890q/android_kernel_samsung_universal8895.git -b lineage-20.0 kernel/samsung/universal8895 --depth 1 --single-branch
+git clone https://github.com/8890q/android_kernel_samsung_universal8895.git -b lineage-21 kernel/samsung/universal8895 --depth 1 --single-branch
 
 # Common tree for Universal8895-common
 rm -rf device/samsung/universal8895-common
-git clone https://github.com/pallavjparikh/android_device_samsung_universal8895-common.git -b elixir-13 device/samsung/universal8895-common --depth 1
+git clone https://github.com/pallavjparikh/android_device_samsung_universal8895-common.git -b elixir-14 device/samsung/universal8895-common --depth 1
 
 # Vendor blobs for Note 8
 rm -rf vendor/samsung
-git clone https://github.com/8890q/proprietary_vendor_samsung.git -b lineage-20.0 vendor/samsung --depth 1
+git clone https://github.com/8890q/proprietary_vendor_samsung.git -b lineage-21.0 vendor/samsung --depth 1
 
 # Hardware OSS parts for Samsung
 mv hardware/samsung/nfc .
 rm -rf hardware/samsung
-git clone https://github.com/ProjectElixir-Devices/hardware_samsung hardware/samsung --depth 1 -b thirteen
+git clone https://github.com/ProjectElixir-Devices/hardware_samsung hardware/samsung --depth 1 -b UNO
 mv nfc hardware/samsung
 
 # SLSI Sepolicy
